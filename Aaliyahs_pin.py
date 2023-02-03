12dc62a95f2966c76df00b536c1381c04e208089
Aaliyah <aaliyahwp1998@gmail.com>
2023-02-03 23:04:01 +0000

    made changes to Aaliyah's pin file

---
+++ Aaliyahs_pin.py
@@ -0,0 +1,11 @@
+import getpass
+
+supplied_pin = getpass.getpass(prompt='Please enter your PIN  ')
+# pin = 1998
+s
+if supplied_pin == '1998':
+    print("Correct pin.")
+else:
+    print("Incorrect pin, please try again.")
+
+
